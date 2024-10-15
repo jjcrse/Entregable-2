04c80a3d828077a2a36c9a572842589dfdb33040
jjcrse <jjcrse452@gmail.com>
2024-10-15 08:52:59 -0500

    Add files via upload

---
+++ Layout/lista1.js
@@ -0,0 +1,137 @@
+let map ={
+    id: 0,
+    name:"Jordan zoom",
+    description:"Court FF ™ Novak celebra el poder de la conexión. Sus colores están inspirados en tejidos individuales en Serbia (patria de Djokovic) para formar un tapiz plano. Tennis Flytefoam ™ entre la comodidad más receptiva y premium en la base del pie, mientras que la tecnología GEL ™ posicionada estratégicamente en el talón y la punta ofrecen amortiguación avanzada en desembarcos difíciles. La rigidez de la tecnología Trustic ™ aumenta la estabilidad y reduce los giros innecesarios, lo que ayuda a mantener los pies fijos durante los movimientos",
+    price:21,
+    image:"https://raw.githubusercontent.com/jjcrse/Entrega-1-pagina-web-/main/Layout/Asics%20Sky%20Elite%20Tokyo.jpeg.jpg",
+    Sizes:[
+        38,39,40
+    ],
+    Color:[
+        "Black","White","Red"
+    ],
+    type:[
+        "Dunk","High","Dunklow"
+    ],
+
+}
+
+let map2={
+    name:"Voleyball Molten",
+    description:"Cubierta: Cuero PUDiseño: 18 PanelesEstructura: Cosido a MaquinaCámara de Aire: ButiloSuperficie: LisaTamaño: Playa Oficial",
+    price:95,
+    image:"https://raw.githubusercontent.com/jjcrse/Entrega-1-pagina-web-/main/Layout/Asics%20Sky%20Elite%20Tokyo.jpeg.jpg",
+    Material:[
+        "Plastic","Leather"
+    ],
+    Color: ["Yellow","Black","Blue"
+    ],      
+    type: [
+        "Beach","Voleyball","Room"
+    ],
+
+}
+
+let map3={
+    name:"Asics Court FF 2 Clay",
+    description:"Court FF ™ Novak celebra el poder de la conexión. Sus colores están inspirados en tejidos individuales en Serbia (patria de Djokovic) para formar un tapiz plano. Tennis Flytefoam ™ entre la comodidad más receptiva y premium en la base del pie, mientras que la tecnología GEL ™ posicionada estratégicamente en el talón y la punta ofrecen amortiguación avanzada en desembarcos difíciles. La rigidez de la tecnología Trustic ™ aumenta la estabilidad y reduce los giros innecesarios, lo que ayuda a mantener los pies fijos durante los movimientos.",
+    price:115,
+    image:"https://raw.githubusercontent.com/jjcrse/Entrega-1-pagina-web-/main/Layout/Adidas%20Crazy%205.jpeg.jpg",
+    Sizes:[
+        40,41,42
+    ],
+    Color:[
+        "Blue","White","Green"
+    ],
+    Position:[
+        "Setter","Middle Blocker","Defensive specialist"
+    ],
+}
+let map4={
+    name:"ASICS GEL-ROCKET 11",
+    description:"El GEL-ROCKET™ 11 es un ZAPATILLA de pista polivalente que ofrece buena estabilidad y libertad de movimiento. ? Este ZAPATILLA fue diseñado con una parte superior flexible, transpirable y más cómoda. Una aplicación TRUSSTIC™ de TPU en la entresuela ayuda a prevenir la torsión y mejora la estabilidad durante las transiciones rápidas. ? Su suela y construcción abrazan el pie para un soporte que ayuda con los movimientos laterales. Al mismo tiempo, las ranuras Flex en la suela garantizan más flexibilidad para una mayor libertad de movimiento en la pista.",
+    price:105,
+    image:"https://raw.githubusercontent.com/jjcrse/Entrega-1-pagina-web-/main/Layout/Camisa%20Col%20azul.jpg",
+    Sizes:[
+            40,41,42
+        ],
+    Color:[
+            "Black","White","Blue"
+        ],
+    Position:[
+            "Setter","Middle Blocker","Defensive specialist"
+        ],
+}
+
+let map5={
+    name:"ASICS GEL-UPCOURT 5",
+    description:"El Upcourt 5 es una zapatilla de deporte ligera para las canchas diseñadas para una mayor flexibilidad y comodidad. Viene con una sección de panel de malla más amplia que ayuda a crear un ajuste más suave y adaptable. Además, las capas de soporte de Mediopé ofrecen una estabilidad mejorada en movimientos multidireccionales. Para colmo, el contrafuerte está reforzado con paneles resistentes que ayudan a aumentar la durabilidad del tenis.",
+    price:105,
+    image:"https://raw.githubusercontent.com/jjcrse/Entrega-1-pagina-web-/main/Layout/Camisa%20azul%20oscuro.jpg",
+    Sizes:[
+            40,41,42
+        ],
+    Color:[
+            "Green","White","Gray"
+        ],
+    Material:[
+            "Plastic","leather"
+        ],
+
+}
+let map6={
+    name:"Gel Upcourt 5",
+    description:"El zapato UPCOURT™ 5 es una oferta de cancha liviana que está diseñada para ofrecer una mejor flexibilidad y un ajuste más cómodo. Cuenta con una sección más amplia de paneles de malla que ayuda a crear un ajuste más suave y adaptable. Mientras tanto, sus superposiciones de apoyo en la parte media del pie ofrecen una mejor estabilidad durante los movimientos multidireccionales. Por último, la puntera y el talón están reforzados con paneles duraderos que ayudan a que sus zapatos duren más.",
+    price:105,
+    image:"https://raw.githubusercontent.com/jjcrse/Entrega-1-pagina-web-/main/Layout/Maletin%20Negro.jpg",
+    Sizes:[
+        40,41,42
+    ],
+    Color:[
+        "Black","White","Blue"
+    ],
+    Position:[
+        "Setter","Middle Blocker","Defensive specialist"
+    ],
+        
+}
+let map7={
+    name:"Rodillera Multiuso M3000",
+    description:"Rodillera Multiuso M3000",
+    price: 206,
+    image:"https://raw.githubusercontent.com/jjcrse/Entrega-1-pagina-web-/main/Layout/Rodilleras.jpeg.jpg",
+    Sizes:[
+        "M","L","XL"
+    ],
+    Color:[
+        "Blue","Gray","Black","White"
+    ],
+
+}
+let map8={
+    name:"Mascara M5700",
+    description:"Mascara grande para protegerse la cara de rebotez",
+    price: 123,
+    image:"https://raw.githubusercontent.com/jjcrse/Entrega-1-pagina-web-/main/Layout/Sin%20mangas%20Col%20amarilla.jpg",
+    Sizes:[
+        "S","M","L"
+    ],
+    Color:[
+        "Blue","Red","White","Black"
+    ],
+
+}
+let map9={
+    name:"Isoc Zapatos Mid",
+    description:"Zapatos de alta calidad que proporcionan una mejor caida al resto y te ayudan a recibir balones mas suavemente y tener mejor arranque",
+    price:167,
+    image:"https://raw.githubusercontent.com/jjcrse/Entrega-1-pagina-web-/main/Layout/Proyecto%20Quitar%20fondo%20(3).png",
+    Sizes:[
+        "36","37","38","39","40","41"
+    ],
+    Color:[
+        "Blue","Red","White","Black","Gray","Red","Yellow"
+    ],
+}
+
+let data = [map,map2,map3,map4,map5,map6,map7,map8,map9]
